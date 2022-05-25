f2c074d789a0c24dee36a8ec64f4c35b4c712a1d
Diegosaurio <diegocarrp@gmail.com>
2022-05-25 08:46:48 -0400

    initial commit

---
+++ src/main/java/cl/diego/balance/users/app/BalanceUsersAppApplication.java
@@ -0,0 +1,13 @@
+package cl.diego.balance.users.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceUsersAppApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( BalanceUsersAppApplication.class, args );
+    }
+
+}
---
+++ src/test/java/cl/diego/balance/users/app/BalanceUsersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.diego.balance.users.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceUsersAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
