92227ba39a3a5f6b0c65af0963429d11967f22d3
Diegosaurio <diegocarrp@gmail.com>
2022-05-26 00:53:25 -0400

    adjusted code style

--- src/main/java/cl/diego/balance/users/app/users/domain/Person.java
+++ src/main/java/cl/diego/balance/users/app/users/domain/Person.java
@@ -1,7 +1,6 @@
 package cl.diego.balance.users.app.users.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/cl/diego/balance/users/app/users/domain/User.java
+++ src/main/java/cl/diego/balance/users/app/users/domain/User.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends Person{
+public class User extends Person {
 
     private String password;
     private String role;
