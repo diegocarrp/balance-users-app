dd0b4833c0bd60599fb815f5b4c5564c54851572
diego <diegocarrp@gmail.com>
2022-07-16 13:00:43 -0400

    implemented customer functionalities

--- src/main/java/cl/diego/balance/users/app/users/repository/domain/Customer.java
+++ src/main/java/cl/diego/balance/users/app/users/repository/domain/Customer.java
@@ -1,5 +1,6 @@
 package cl.diego.balance.users.app.users.repository.domain;
 
+import cl.diego.balance.users.app.users.dto.CustomerDto;
 import lombok.*;
 
 import javax.persistence.*;
@@ -23,5 +24,28 @@ public class Customer {
     private String lastname1;
     private String lastname2;
     private String cellphone;
+
+    public Customer( CustomerDto customer ) {
+        this.rut       = customer.getRut( );
+        this.id        = customer.getId( );
+        this.email     = customer.getEmail( );
+        this.names     = customer.getNames( );
+        this.lastname1 = customer.getLastname1( );
+        this.lastname2 = customer.getLastname2( );
+        this.cellphone = customer.getCellphone( );
+    }
+
+    public CustomerDto toCustomer( ) {
+
+        return CustomerDto.builder()
+                .id( this.id )
+                .rut( rut )
+                .email( this.email )
+                .names( this.names )
+                .lastname1( this.lastname1 )
+                .lastname2( this.lastname2 )
+                .cellphone( this.cellphone )
+                .build();
+    }
 
 }
